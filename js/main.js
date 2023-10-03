191f3bf16cf2a0f7e2a5971c55f7a65c3ca5ee10
Max <m.gagner@hotmail.se>
2023-10-03 13:17:29 +0200

    Added list creation on site load aswell as a clear, most logic is done.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 "use strict"
 
 //variables
+let todoItems = ["dwa", "nan"];
 let newTodoInput;
 let saveButton = document.getElementById("newtodobutton");
+let todoList = document.getElementById("todolist");
+let clear = document.getElementById("clearbutton");
 
 //on button press function
 saveButton.addEventListener("click", function(){
@@ -14,5 +17,23 @@ saveButton.addEventListener("click", function(){
         return;
     }
 
-    console.log(newTodoInput);
+    todoItems.push(newTodoInput);
+    console.log(todoItems);
+    location.reload();
+});
+
+//loads the todolist on load
+window.addEventListener("load", function() { 
+    //creates a article for every value in todoItems and inserts those under todo
+    for (let index = 0; index < todoItems.length; index++) {
+        let newTodoItem = document.createElement("article");
+        newTodoItem.innerHTML = todoItems[index];
+        todoList.appendChild(newTodoItem);
+    }
+});
+
+//clear button
+clear.addEventListener("click", function(){
+    todoItems = [];
+    location.reload();
 });
