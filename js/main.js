21f1df45a43dd41eabd040da0e9028ffe51fcf6c
Max <m.gagner@hotmail.se>
2023-10-03 12:45:18 +0200

    Gets the input properly.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-/*
-Här lägger du din JavaScript-kod
-*/
+"use strict"
+
+//variables
+let newTodoInput;
+let saveButton = document.getElementById("newtodobutton");
+
+//on button press function
+saveButton.addEventListener("click", function(){
+    //gets value of field
+    newTodoInput = document.getElementById("newtodo").value;
+
+    //if value is empty exit function
+    if (!newTodoInput) {
+        return;
+    }
+
+    console.log(newTodoInput);
+});
